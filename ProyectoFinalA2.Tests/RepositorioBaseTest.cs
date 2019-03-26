8f22914a9d087eb42ba3b43925f4cb7d06dde917
luis lorenzo <sixsixonelui@gmail.com>
2019-03-26 03:06:44 -0400

    unitTest

---
+++ ProyectoFinalA2.Tests/RepositorioBaseTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BLL;
+using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProyectoFinalA2.Tests
+{
+    [TestClass]
+    public class RepositorioBaseTest
+    {
+        RepositorioBase<Usuario> repositorio = new RepositorioBase<Usuario>();
+        Usuario usuario = new Usuario();
+
+        [TestMethod]
+        public void Guardar()
+        {
+            Assert.IsTrue(repositorio.Guardar(usuario));
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            var usuario_var = repositorio.Buscar(1);
+
+            Assert.IsNotNull(usuario_var);
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+            repositorio.Guardar(usuario);
+            usuario.Nombre = "Prueba";
+            Assert.IsTrue(repositorio.Modificar(usuario));
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            Assert.IsTrue(repositorio.Eliminar(1));
+        }
+    }
+}
