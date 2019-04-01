4437a8899f028866bc7180b161f19bde0c3f143f
luis lorenzo <sixsixonelui@gmail.com>
2019-04-01 02:08:29 -0400

    cambios

---
+++ ProyectoFinalA2/Welcom.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoFinalA2
+{
+    public partial class Welcom : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
